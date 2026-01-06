15ec8a48e0ef65a12fa8b21768921aee91732023
datastory <rpate226@asu.edu>
2026-01-06 15:03:38 -0700

    I changed .png to .jpg in images

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function showBusiness() {
-    document.getElementById("heroImage").src = "images/business.png";
+    document.getElementById("heroImage").src = "images/business.jpg";
 }
 
 function showEngineer() {
-    document.getElementById("heroImage").src = "images/engineer.png";
+    document.getElementById("heroImage").src = "images/engineer.jpg";
 }
 
 function showNeutral() {
-    document.getElementById("heroImage").src = "images/neutral.png";
+    document.getElementById("heroImage").src = "images/neutral.jpg";
 }
