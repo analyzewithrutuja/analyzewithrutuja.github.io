a113821dd95c7ee7cdae10a10ac4dca95eb44f51
datastory <rpate226@asu.edu>
2026-01-06 23:07:36 -0700

    made few changes

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function showBusiness() {
-    document.getElementById("heroImage").src = "images/business.jpg";
+    document.getElementById("imgNeutral").classList.remove("active");
+    document.getElementById("imgEngineer").classList.remove("active");
+    document.getElementById("imgBusiness").classList.add("active");
 }
 
 function showEngineer() {
-    document.getElementById("heroImage").src = "images/engineer.jpg";
+    document.getElementById("imgNeutral").classList.remove("active");
+    document.getElementById("imgBusiness").classList.remove("active");
+    document.getElementById("imgEngineer").classList.add("active");
 }
 
 function showNeutral() {
-    document.getElementById("heroImage").src = "images/neutral.jpg";
+    document.getElementById("imgBusiness").classList.remove("active");
+    document.getElementById("imgEngineer").classList.remove("active");
+    document.getElementById("imgNeutral").classList.add("active");
 }
