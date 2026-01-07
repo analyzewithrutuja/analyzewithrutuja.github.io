47cc82b99dfb073c7330f0a30a0d6067a00d9824
datastory <rpate226@asu.edu>
2026-01-07 16:01:46 -0700

    trying to add the hover screen

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
+function showNeutral() {
+    document.getElementById("imgNeutral").classList.add("active");
+    document.getElementById("imgBusiness").classList.remove("active");
+    document.getElementById("imgEngineer").classList.remove("active");
+}
+
 function showBusiness() {
     document.getElementById("imgNeutral").classList.remove("active");
-    document.getElementById("imgEngineer").classList.remove("active");
     document.getElementById("imgBusiness").classList.add("active");
+    document.getElementById("imgEngineer").classList.remove("active");
 }
 
 function showEngineer() {
     document.getElementById("imgNeutral").classList.remove("active");
     document.getElementById("imgBusiness").classList.remove("active");
     document.getElementById("imgEngineer").classList.add("active");
-}
-
-function showNeutral() {
-    document.getElementById("imgBusiness").classList.remove("active");
-    document.getElementById("imgEngineer").classList.remove("active");
-    document.getElementById("imgNeutral").classList.add("active");
 }
