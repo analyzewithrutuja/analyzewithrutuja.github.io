15d4accc25d4164f898091247c9465602e03bf69
datastory <rpate226@asu.edu>
2026-01-07 16:13:42 -0700

    the previous hover was wrong so I added the new one

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function showEngineer() {
     document.getElementById("imgBusiness").classList.remove("active");
     document.getElementById("imgEngineer").classList.add("active");
 }
+
+
+function hoverLeft() {
+    document.querySelector(".bg-left").style.width = "60%";
+    document.querySelector(".bg-right").style.width = "40%";
+    showBusiness();
+}
+
+function hoverRight() {
+    document.querySelector(".bg-right").style.width = "60%";
+    document.querySelector(".bg-left").style.width = "40%";
+    showEngineer();
+}
+
+function hoverNeutral() {
+    document.querySelector(".bg-left").style.width = "50%";
+    document.querySelector(".bg-right").style.width = "50%";
+    showNeutral();
+}
